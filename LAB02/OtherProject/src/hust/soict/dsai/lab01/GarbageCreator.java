9d87fa6eb9470435da3a2d3c434beb07a5429f73
Luf123B57t <dinhtuanminh1509hp@gmail.com>
2025-10-24 21:46:05 +0700

    Add String, StringBuilder, StringBuffer

---
+++ LAB02/OtherProject/src/hust/soict/dsai/lab01/GarbageCreator.java
@@ -0,0 +1,21 @@
+package hust.soict.dsai.lab01;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class GarbageCreator {
+    public static void main(String[] args) throws Exception {
+        String filename = "D:\\Java\\LAB_ALL\\757052-DinhTuanMinh-202416721\\LAB02\\OtherProject\\large.txt";
+        byte[] inputBytes = { };
+        long startTime, endTime;
+
+        inputBytes = Files.readAllBytes(Paths.get(filename));
+        startTime = System.currentTimeMillis();
+        String outputString = "";
+        for (byte b : inputBytes) {
+            outputString += (char) b;
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+    }
+}
+
